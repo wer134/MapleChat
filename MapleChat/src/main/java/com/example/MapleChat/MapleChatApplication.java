2440ad4dab1c0f50cbf9b98b5859a8f63ec4fd5d
wer134 <87606833+wer134@users.noreply.github.com>
2026-01-19 15:41:57 +0900

    kim : Springboot Setting

---
+++ MapleChat/src/main/java/com/example/MapleChat/MapleChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.MapleChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapleChatApplication.class, args);
+	}
+
+}
